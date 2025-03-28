f70136e63bf33a7762f97d83d00d1c30fb045b1e
ZackZoa <144346012+ZackZoa@users.noreply.github.com>
2025-03-28 11:50:00 +0100

    so far today

--- index.js
+++ index.js
@@ -4,21 +4,42 @@ const canvas = document.querySelector('canvas')
     canvas.width = 64 * 16 //1024
     canvas.height = 64 * 9 //576
 
-    let y = 100
-    const height = 100
-    let bottom = y + 100
+    class player {
+      constructor(){
+        this.position = {
+          x: 100,
+          y: 100
+        }
+
+        this.width = 100
+        this.height = 100
+        this.sides = {
+          bottom: this.position.y + this.height
+        }
+      }
+
+      draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+      }
+
+      update() {
+        if (this.sides.bottom < canvas.height) {
+            this.position.y++
+            this.sides.bottom = this.position.y + this.height
+         }
+      }
+    }
+
+    const player = new player()
+
+    // let bottom = y + 100
     function animate() {
      window.requestAnimationFrame(animate)
      c.fillStyle = 'white'
      c.fillRect(0, 0, canvas.width, canvas.height)
 
-     c.fillStyle = 'red'
-     c.fillRect(100, y, 100, height)
-
-     if (bottom < canvas.height) {
-       y++
-       bottom = y + 100
-    }
+      player.draw()
 }
 
 animate()
