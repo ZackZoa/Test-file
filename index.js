c416128cd4e887f07ae903fac30a7ab80f153c21
ZackZoa <144346012+ZackZoa@users.noreply.github.com>
2025-04-02 11:30:43 +0200

    Bckground images changes but does not work

--- index.js
+++ index.js
@@ -4,33 +4,49 @@ const canvas = document.querySelector('canvas')
     canvas.width = 64 * 16 //1024
     canvas.height = 64 * 9 //576
 
+    class Sprite {
+      constructor ({position}) {
+        this.position = position
+        this.image = new Image()
+        this.image.src = './image/backgroundLevel1.png'
+      }
+      draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+      }
+    }
+
+  const backgroundLevel1 = new Sprite ({
+    position: {
+      x: 0,
+      y: 0,
+    }
+  })
+
   const player = new Player()
 
-    // let bottom = y + 100
+  const keys = {
+    w: {
+      pressed: false,
+    },
+    a: {
+      pressed: false,
+    },
+    d: {
+      pressed: false,
+    },
+
+  }
   function animate() {
      window.requestAnimationFrame(animate)
-     c.fillStyle = 'white'
-     c.fillRect(0, 0, canvas.width, canvas.height)
+
+     backgroundLevel1.draw()
+
+      player.velocity.x = 0
+      if (keys.d.pressed) player.velocity.x = 5
+      else if (keys.a.pressed) player.velocity.x = -5
 
       player.draw()
       player.update()
 }
 
 animate()
-
-window.addEventListener('keydown', (event) => {
-  switch (event.key){
-    case 'w':
-      if (player.velocity.y === 0) player.velocity.y = -20
-
-      break
-    case 'a':
-      //move player to the left
-      player.velocity.x = -4
-      break
-    case 'd':
-      //move player to the right
-      player.velocity.x = 4
-      break
-  }
-})
